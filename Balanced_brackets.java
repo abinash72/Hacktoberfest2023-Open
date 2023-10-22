ea543ef879db3b1513ee46db76e0b74d0d1303e4
Abinash Sahu <107759499+abinash72@users.noreply.github.com>
2023-10-22 10:03:47 +0530

    Create Balanced_brackets.java

---
+++ Balanced_brackets.java
@@ -0,0 +1,33 @@
+//program to check the brackets are balanced or not using queue data structure in java
+public class GFG {
+	public static boolean areBracketsBalanced(String s)
+	{
+		int i = -1;
+		char[] stack = new char[s.length()];
+		for (char ch : s.toCharArray()) {
+			if (ch == '(' || ch == '{' || ch == '[')
+				stack[++i] = ch;
+			else {
+				if (i >= 0
+					&& ((stack[i] == '(' && ch == ')')
+						|| (stack[i] == '{' && ch == '}')
+						|| (stack[i] == '[' && ch == ']')))
+					i--;
+				else
+					return false;
+			}
+		}
+		return i == -1;
+	}
+
+	public static void main(String[] args)
+	{
+		String expr = "{()}[]";
+
+		// Function call
+		if (areBracketsBalanced(expr))
+			System.out.println("Balanced");
+		else
+			System.out.println("Not Balanced");
+	}
+}
